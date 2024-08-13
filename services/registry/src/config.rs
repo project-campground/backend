b6e7ddbf582b3d0084df988d180a4d59b9e736ef
Reapimus <29674188+Reapimus@users.noreply.github.com>
2024-08-13 22:46:30 +1000

    Update config structs

--- services/registry/src/config.rs
+++ services/registry/src/config.rs
@@ -1,5 +1,5 @@
 use rocket::serde::Deserialize;
-
+use did_method_plc::Keypair;
 
 #[derive(Debug, Deserialize, Clone)]
 #[serde(crate = "rocket::serde")]
@@ -7,9 +7,22 @@ pub struct AuthConfig {
     pub secret_key: String,
 }
 
+impl AuthConfig {
+    pub fn secret_key(&self) -> Keypair {
+        Keypair::from_private_key(&self.secret_key).unwrap()
+    }
+}
+
 #[derive(Debug, Deserialize, Clone)]
 #[serde(crate = "rocket::serde")]
 pub struct ServiceConfig {
     pub public_url: String,
     pub did: String,
+    pub secret_key: String,
+}
+
+impl ServiceConfig {
+    pub fn secret_key(&self) -> Keypair {
+        Keypair::from_private_key(&self.secret_key).unwrap()
+    }
 }
