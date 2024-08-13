3db4b62dbb7b5a3a5a58b9cd1dd074f60e561d49
Reapimus <29674188+Reapimus@users.noreply.github.com>
2024-08-13 22:50:03 +1000

    More WIP DB models

--- services/registry/src/database/models.rs
+++ services/registry/src/database/models.rs
@@ -1,13 +1,54 @@
 use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
+use surrealdb::sql::Thing;
 
-#[derive(Serialize, Deserialize)]
-pub struct User {
-    pub id: String,
-    pub created_at: DateTime<Utc>,
-
-    pub handle: String,
-    pub email: Option<String>,
-    pub display_name: String,
-    pub avatar_url: String,
+pub trait DBModel {
+    fn id(&self) -> String;
+    fn table(&self) -> String;
 }
+
+macro_rules! impl_db_model {
+    ($($types:ident),*) => {
+        $(
+            impl DBModel for $types {
+                fn id(&self) -> String {
+                    self.id.id.to_string()
+                }
+
+                fn table(&self) -> String {
+                    self.id.tb.to_string()
+                }
+            }
+        )*
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Account {
+    id: Thing,
+    created_at: DateTime<Utc>,
+    email: String,
+    password: String,
+    email_confirmed_at: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Actor {
+    id: Thing,
+    handle: String,
+    created_at: DateTime<Utc>,
+    deactivated_at: Option<DateTime<Utc>>,
+    delete_after: Option<DateTime<Utc>>,
+    takedown_ref: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AppPassword {
+    id: Thing,
+    name: String,
+    created_at: DateTime<Utc>,
+    password: String,
+    privileged: bool,
+}
+
+impl_db_model![Account, Actor, AppPassword];
