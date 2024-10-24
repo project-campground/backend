738b4a6a2092be9f07550c8f74daf2f09f2561f4
Reapimus <29674188+Reapimus@users.noreply.github.com>
2024-10-24 23:49:02 +1100

    Update DB Schemas

--- services/registry/src/database/models.rs
+++ services/registry/src/database/models.rs
@@ -1,29 +1,10 @@
+#![allow(dead_code, unused_imports)]
 use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 use surrealdb::sql::Thing;
 
-pub trait DBModel {
-    fn id(&self) -> String;
-    fn table(&self) -> String;
-}
-
-macro_rules! impl_db_model {
-    ($($type:ident),*) => {
-        $(
-            impl DBModel for $type {
-                fn id(&self) -> String {
-                    self.id.id.to_string()
-                }
-
-                fn table(&self) -> String {
-                    self.id.tb.to_string()
-                }
-            }
-        )*
-    }
-}
-
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Account {
     pub id: Thing,
     pub created_at: DateTime<Utc>,
@@ -32,23 +13,75 @@ pub struct Account {
     pub email_confirmed_at: Option<DateTime<Utc>>,
 }
 
+impl Account {
+    pub fn did(&self) -> String {
+        self.id.id.to_string()
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Actor {
+#[serde(rename_all = "camelCase")]
+pub struct RepoRoot {
     pub id: Thing,
-    pub handle: String,
-    pub created_at: DateTime<Utc>,
-    pub deactivated_at: Option<DateTime<Utc>>,
-    pub delete_after: Option<DateTime<Utc>>,
+    pub cid: String,
+    pub rev: String,
+    pub indexed_at: DateTime<Utc>,
+}
+
+impl RepoRoot {
+    pub fn did(&self) -> String {
+        self.id.id.to_string()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RepoBlock {
+    pub did: String,
+    pub cid: String,
+    pub repo_rev: String,
+    pub size: i32,
+    pub content: Vec<u8>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Record {
+    pub id: Thing,
+    pub did: String,
+    pub cid: String,
+    pub collection: String,
+    pub rkey: String,
+    pub repo_rev: String,
+    pub indexed_at: DateTime<Utc>,
     pub takedown_ref: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct AppPassword {
-    pub id: Thing,
-    pub name: String,
-    pub created_at: DateTime<Utc>,
-    pub password: String,
-    pub privileged: bool,
+impl Record {
+    pub fn uri(&self) -> String {
+        self.id.id.to_string()
+    }
 }
 
-impl_db_model![Account, Actor, AppPassword];
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Blob {
+    pub id: Thing,
+    pub did: String,
+    pub cid: String,
+    pub mime_type: String,
+    pub size: i32,
+    pub temp_key: Option<String>,
+    pub width: i32,
+    pub height: i32,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RecordBlob {
+    pub id: Thing,
+    pub record_uri: String,
+    pub blob_uri: String,
+    pub did: String,
+}
