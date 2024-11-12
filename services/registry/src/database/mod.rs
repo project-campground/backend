09b097e13b9f25f5d8e58ba4ea81eb95be821992
Reapimus <29674188+Reapimus@users.noreply.github.com>
2024-11-13 00:29:32 +1100

    Update config

--- services/registry/src/database/mod.rs
+++ services/registry/src/database/mod.rs
@@ -1,8 +1,7 @@
 use std::sync::{Mutex, OnceLock};
 
 use anyhow::Result;
-use rocket::Config;
-use crate::config::DatabaseConfig;
+use crate::config::DATABASE_CONFIG;
 use diesel::{pg::PgConnection, r2d2::{Pool, ConnectionManager, PooledConnection}};
 use lazy_static::lazy_static;
 
@@ -13,19 +12,15 @@ pub type DbPool = Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
 lazy_static! {
-    static ref CONFIG: DatabaseConfig = Config::figment()
-        .extract_inner("database")
-        .expect("Failed to load database configuration");
-
     static ref POOL: OnceLock<Mutex<DbPool>> = OnceLock::new();
 }
 
 pub fn establish_connection() -> Result<DbConnection> {
     let pool = POOL.get_or_init(|| {
-        let manager = ConnectionManager::<PgConnection>::new(&CONFIG.url);
+        let manager = ConnectionManager::<PgConnection>::new(&DATABASE_CONFIG.url);
         Mutex::new(
             Pool::builder()
-                .max_size(CONFIG.pool_size.clone())
+                .max_size(DATABASE_CONFIG.pool_size.clone())
                 .build(manager)
                 .expect("Failed to create connection pool")
             )
