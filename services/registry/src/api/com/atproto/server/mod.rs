1622483650c58511f3af316924651cfe8727f029
Reapimus <29674188+Reapimus@users.noreply.github.com>
2024-11-15 23:58:09 +1100

    Implement com.atproto.server.deleteSession

--- services/registry/src/api/com/atproto/server/mod.rs
+++ services/registry/src/api/com/atproto/server/mod.rs
@@ -285,5 +285,6 @@ pub async fn assert_valid_doc_contents(contents: AssertionContents) -> Result<()
 
 pub mod create_account;
 pub mod create_session;
+pub mod delete_session;
 pub mod get_session;
 pub mod refresh_session;
