e6cc77a696a899bf7cd27a50cf53403f60d77e99
Reapimus <29674188+Reapimus@users.noreply.github.com>
2024-11-17 01:12:57 +1100

    Implement com.atproto.server.updateEmail

--- services/registry/src/api/com/atproto/server/mod.rs
+++ services/registry/src/api/com/atproto/server/mod.rs
@@ -294,6 +294,7 @@ pub mod refresh_session;
 pub mod get_service_auth;
 pub mod describe_server;
 pub mod reset_password;
+pub mod update_email;
 pub mod request_email_confirmation;
 pub mod request_password_reset;
 pub mod request_email_update;
