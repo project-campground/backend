eecf2c796fd401d3c25724542f655bfb011fd252
PrettyGoodName <31628630+IdkGoodName@users.noreply.github.com>
2026-08-24 19:53:30 +0300

    feat(appview): add fetching specific actor campsites

--- services/appview/src/api/gg/campground/campsite/get_actor_campsites.rs
+++ services/appview/src/api/gg/campground/campsite/get_actor_campsites.rs
@@ -8,36 +8,61 @@ use rocket::{State, serde::json::Json};
 use crate::{
     database::{actors::get_actor, establish_connection},
     helpers::api::handle_all_db_errors,
-    views::
-        campsites::campsite_view_basic
-    ,
+    views::campsites::campsite_view_basic,
     xrpc::{
         auth::Authorization,
         error::{Result, XRPCError},
     },
 };
 
-#[get("/xrpc/gg.campground.campsite.getActorCampsites")]
+#[get("/xrpc/gg.campground.campsite.getActorCampsites", rank = 1)]
 pub async fn get_actor_campsites(
     auth: Authorization<'_>,
     client: &State<Client>,
     did_document_storage: &State<LruDidDocumentStorage>,
 ) -> Result<Json<GetCampsitesOutput>> {
-    let mut conn = establish_connection().unwrap();
-
-    let actor =
-        get_actor(client, did_document_storage, &auth.actor_did)
-            .await
-            .map_err(|_| XRPCError::Unauthorized)?;
+    let actor = get_actor(client, did_document_storage, &auth.actor_did)
+        .await
+        .map_err(|_| XRPCError::Unauthorized)?;
 
     let campsite_ids_filtered: Vec<String> =
         actor.campsites.iter().filter_map(|x| x.clone()).collect();
 
-    let campsites = if actor.campsites.is_empty() {
+    return get_actor_campsites_from_list(campsite_ids_filtered);
+}
+
+#[get("/xrpc/gg.campground.campsite.getActorCampsites?<campsites>", rank = 2)]
+pub async fn get_actor_specific_campsites(
+    auth: Authorization<'_>,
+    client: &State<Client>,
+    did_document_storage: &State<LruDidDocumentStorage>,
+    campsites: Vec<&str>,
+) -> Result<Json<GetCampsitesOutput>> {
+    let actor = get_actor(client, did_document_storage, &auth.actor_did)
+        .await
+        .map_err(|_| XRPCError::Unauthorized)?;
+
+    let campsite_ids_filtered: Vec<String> = actor
+        .campsites
+        .iter()
+        .filter(|x| {
+            x.as_ref()
+                .map_or(false, |x| campsites.contains(&x.as_str()))
+        })
+        .map(|x| x.as_ref().unwrap().clone())
+        .collect();
+
+    return get_actor_campsites_from_list(campsite_ids_filtered);
+}
+
+fn get_actor_campsites_from_list(campsite_ids: Vec<String>) -> Result<Json<GetCampsitesOutput>> {
+    let mut conn = establish_connection().unwrap();
+
+    let campsites = if campsite_ids.is_empty() {
         vec![]
     } else {
         crate::schema::appview::campsite::table
-            .filter(crate::schema::appview::campsite::id.eq_any(campsite_ids_filtered))
+            .filter(crate::schema::appview::campsite::id.eq_any(campsite_ids))
             .load::<Campsite>(&mut conn)
             .map_err(handle_all_db_errors)?
             .iter()
@@ -45,7 +70,5 @@ pub async fn get_actor_campsites(
             .collect::<Vec<CampsiteViewBasic>>()
     };
 
-    return Ok(Json(GetCampsitesOutput {
-        campsites,
-    }));
+    return Ok(Json(GetCampsitesOutput { campsites }));
 }
--- services/appview/src/api/gg/campground/campsite/mod.rs
+++ services/appview/src/api/gg/campground/campsite/mod.rs
@@ -2,6 +2,7 @@ pub fn routes() -> Vec<rocket::Route> {
     routes![
         get_campsites::get_campsites,
         get_actor_campsites::get_actor_campsites,
+        get_actor_campsites::get_actor_specific_campsites,
         get_campsite::get_campsite,
         create_campsite::create_campsite,
         update_campsite::update_campsite,
@@ -9,9 +10,9 @@ pub fn routes() -> Vec<rocket::Route> {
     ]
 }
 
-mod get_actor_campsites;
-mod get_campsites;
-mod get_campsite;
 mod create_campsite;
-mod update_campsite;
 mod delete_campsite;
+mod get_actor_campsites;
+mod get_campsite;
+mod get_campsites;
+mod update_campsite;
